abaea73b815920e0b5429695f8ab0751b4cbc9ba
hustlei <hustlei@sina.cn>
2016-12-17 23:16:27 +0800

    min change my

--- my/js/gcard.js
+++ my/js/gcard.js
@@ -4,7 +4,7 @@ var gname=glist[i].name;
 var a=glist[i].album;
 for(var j in a)
 {
-document.write('<div class="col-sm-3 col-xs-4 card px-0">');
+document.write('<div class="col-md-3 col-sm-4 col-xs-6 card px-0">');
 document.write('<a href="girl.html?i='+i+'&j='+j+'">');
 document.write('<img class="card-img-top w-100" src="'+root+a[j].img[0]+'">'); 
 document.write('</a><div class="card-block text-xs-center">');
