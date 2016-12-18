9d3b22ef0935cbe408f0652928d749dbe1b20de4
hustlei <hustlei@sina.cn>
2016-12-18 22:36:09 +0800

    fixed soft carousel,change girlcard to div

--- my/js/gcard.js
+++ my/js/gcard.js
@@ -4,13 +4,13 @@ var gname=glist[i].name;
 var a=glist[i].album;
 for(var j in a)
 {
-document.write('<div class="col-md-3 col-sm-4 col-xs-6 card px-0">');
+document.write('<div class="col-md-3 col-sm-4 col-xs-6">');
 document.write('<a href="girl.html?i='+i+'&j='+j+'">');
-document.write('<img class="card-img-top w-100" src="'+root+a[j].img[0]+'">'); 
-document.write('</a><div class="card-block text-xs-center">');
+document.write('<img class="w-100" src="'+root+a[j].img[0]+'">'); 
+document.write('</a><p class="text-xs-center">');
 document.write('<a href="girl.html?i='+i+'&j='+j+'">');
 if(a[j].name != "default")document.write(a[j].name+' - ');
 document.write(gname);
-document.write('</a></div></div>');
+document.write('</a></p></div>');
 }
 }
