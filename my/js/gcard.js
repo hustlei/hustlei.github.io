eef2b5c2dde32fc002139a880bd9011213ee26b5
hustlei <hustlei@sina.cn>
2017-01-17 21:34:22 +0800

    修改js 输出html方式
    document.write()修改为getElementById("").innerHTML=“”

--- my/js/gcard.js
+++ my/js/gcard.js
@@ -1,16 +1,18 @@
+var card="";
 for(var i in glist)
 {
 var gname=glist[i].name;
 var a=glist[i].album;
 for(var j in a)
 {
-document.write('<div class="col-md-3 col-sm-4 col-xs-6">');
-document.write('<a href="girl.html?i='+i+'&j='+j+'">');
-document.write('<img class="w-100" src="'+root+a[j].img[0]+'">'); 
-document.write('</a><p class="text-xs-center">');
-document.write('<a href="girl.html?i='+i+'&j='+j+'">');
-if(a[j].name != "default")document.write(a[j].name+' - ');
-document.write(gname);
-document.write('</a></p></div>');
+card+='<div class="col-md-3 col-sm-4 col-xs-6">';
+card+='<a href="girl.html?i='+i+'&j='+j+'">';
+card+='<img class="w-100" src="'+root+a[j].img[0]+'">'; 
+card+='</a><p class="text-xs-center">';
+card+='<a href="girl.html?i='+i+'&j='+j+'">';
+if(a[j].name != "default")card+=a[j].name+' - ';
+card+=gname;
+card+='</a></p></div>';
 }
 }
+document.getElementById("girls").innerHTML=card;
